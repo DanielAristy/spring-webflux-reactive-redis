8b5d98f5fb0bc59d6037291489563480f62a5bbb
daniel.aristizabal <daniel.aristizabal@sofka.com.co>
2023-09-18 18:43:35 -0500

    Se implementa el manejo de errores en la respuesta

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/example/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/example/api/Handler.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -21,26 +23,39 @@ public class Handler {
 
         return serverRequest.bodyToMono(Book.class)
                 .flatMap(bookUseCase::create)
-                .flatMap(book -> ServerResponse.ok().bodyValue(book));
+                .flatMap(book -> ServerResponse.ok().bodyValue(book))
+                .onErrorResume(throwable -> getResponseError(throwable.getMessage()));
     }
 
     public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
 
         return Mono.just(Objects.requireNonNull(serverRequest.pathVariable("id")))
                 .flatMap(bookUseCase::get)
-                .flatMap(book -> ServerResponse.ok().bodyValue(book));
+                .flatMap(book -> ServerResponse.ok().bodyValue(book))
+                .onErrorResume(throwable -> Mono.just(throwable.getMessage())
+                        .flatMap(Handler::getResponseError)
+                );
+    }
+
+    private static Mono<ServerResponse> getResponseError(String message) {
+        Map<String, String> error = new HashMap<>();
+        error.put("id:", UUID.randomUUID().toString());
+        error.put("response:", message);
+        error.put("status:", "500");
+        return ServerResponse.status(500).bodyValue(error);
     }
 
     public Mono<ServerResponse> listenGETALLUseCase(ServerRequest serverRequest) {
-        Flux<Book> books = bookUseCase.getAll();
-        return ServerResponse.ok().body(books, Book.class);
-
+        return bookUseCase.getAll()
+                .collectList().flatMap(books -> ServerResponse.ok().bodyValue(books))
+                .onErrorResume(throwable -> getResponseError(throwable.getMessage()));
     }
 
     public Mono<ServerResponse> listenDELETEUseCase(ServerRequest serverRequest) {
         return Mono.just(Objects.requireNonNull(serverRequest.pathVariable("id")))
                 .flatMap(bookUseCase::deleteById)
-                .flatMap(value -> ServerResponse.ok().bodyValue(value));
+                .flatMap(value -> ServerResponse.ok().bodyValue(value))
+                .onErrorResume(throwable -> getResponseError(throwable.getMessage()));
 
     }
 }
